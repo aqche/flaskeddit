3536e13a13c0b1a824f23912a467512f6eb9b9db
Austin <cheungaustin2@gmail.com>
2019-03-29 21:59:34 -0500

    add titles to login/register form, use url_for for redirects, add logout flash

--- flaskeddit/auth/routes.py
+++ flaskeddit/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, render_template
+from flask import flash, redirect, render_template, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
 from flaskeddit import bcrypt, db
@@ -10,7 +10,7 @@ from flaskeddit.models import User
 @auth_blueprint.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect("/")
+        return redirect(url_for("feed.feed"))
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
@@ -18,24 +18,24 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash("Successfully Registered", "primary")
-        return redirect("/login")
+        return redirect(url_for("auth.login"))
     return render_template("register.jinja2", form=form)
 
 
 @auth_blueprint.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect("/")
+        return redirect(url_for("feed.feed"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
             flash("Successfully Logged In", "primary")
-            return redirect("/")
+            return redirect(url_for("feed.feed"))
         else:
             flash("Login Failed", "danger")
-            return redirect("/login")
+            return redirect(url_for("auth.login"))
     return render_template("login.jinja2", form=form)
 
 
@@ -43,4 +43,5 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect("/login")
+    flash("Successfully Logged Out", "primary")
+    return redirect(url_for("auth.login"))
