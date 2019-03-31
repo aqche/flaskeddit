5764b31a3175e8cbeabcb210ce3c86ceea3c215c
Austin <cheungaustin2@gmail.com>
2019-03-31 13:31:49 -0500

    update models

--- flaskeddit/models.py
+++ flaskeddit/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask_login import UserMixin
 
 from flaskeddit import db, login_manager
@@ -14,4 +16,50 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
-        return "<User (id='{0}', username='{1}')".format(self.id, self.username)
+        return "<User (username='{0}')>".format(self.username)
+
+
+class Community(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date_created = db.Column(
+        db.DateTime, nullable=False, default=datetime.datetime.utcnow
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return "<Community (name='{0}', description='{1}', date_created='{3}')>".format(
+            self.name, self.description, self.date_created
+        )
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    post = db.Column(db.Text, nullable=False)
+    date_created = db.Column(
+        db.DateTime, nullable=False, default=datetime.datetime.utcnow
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    community_id = db.Column(db.Integer, db.ForeignKey("community.id"), nullable=False)
+
+    def __repr__(self):
+        return "<Post (title='{0}', post='{1}', date_created='{3}')>".format(
+            self.title, self.post, self.date_created
+        )
+
+
+class Reply(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    reply = db.Column(db.Text, nullable=False)
+    date_created = db.Column(
+        db.DateTime, nullable=False, default=datetime.datetime.utcnow
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+
+    def __repr__(self):
+        return "<Reply (reply='{0}', date_created='{2}')>".format(
+            self.reply, self.date_created
+        )
