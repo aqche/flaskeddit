3ca8cb19101d0fddfb5cdd5f0621fa9842119812
Austin <cheungaustin2@gmail.com>
2019-04-02 21:37:38 -0500

    use f strings, create basic communities page

--- flaskeddit/communities/routes.py
+++ flaskeddit/communities/routes.py
@@ -1,9 +1,13 @@
+from flask import render_template
+
 from flaskeddit.communities import communities_blueprint
+from flaskeddit.models import Community
 
 
 @communities_blueprint.route("/communities")
 def communities():
-    return "Recently Updated Communities"
+    communities = Community.query.order_by(Community.date_created.desc()).all()
+    return render_template("communities.jinja2", communities=communities)
 
 
 @communities_blueprint.route("/communities/top")
--- flaskeddit/models.py
+++ flaskeddit/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
-        return "<User (username='{0}')>".format(self.username)
+        return f"<User (username='{self.username}')>"
 
 
 class Community(db.Model):
@@ -29,9 +29,7 @@ class Community(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     def __repr__(self):
-        return "<Community (name='{0}', description='{1}', date_created='{2}')>".format(
-            self.name, self.description, self.date_created
-        )
+        return f"<Community (name='{self.name}', description='{self.description}', date_created='{self.date_created}')>"
 
 
 class Post(db.Model):
@@ -45,9 +43,7 @@ class Post(db.Model):
     community_id = db.Column(db.Integer, db.ForeignKey("community.id"), nullable=False)
 
     def __repr__(self):
-        return "<Post (title='{0}', post='{1}', date_created='{2}')>".format(
-            self.title, self.post, self.date_created
-        )
+        return f"<Post (title='{self.title}', post='{self.post}', date_created='{self.date_created}')>"
 
 
 class Reply(db.Model):
@@ -60,6 +56,4 @@ class Reply(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
 
     def __repr__(self):
-        return "<Reply (reply='{0}', date_created='{1}')>".format(
-            self.reply, self.date_created
-        )
+        return f"<Reply (reply='{self.reply}', date_created='{self.date_created}')>"
