6e2a16f73f76902668d60433b8b193669e41743d
Austin <cheungaustin2@gmail.com>
2019-04-04 22:28:19 -0500

    add pagination for communities

--- flaskeddit/communities/routes.py
+++ flaskeddit/communities/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 
 from flaskeddit import db
 from flaskeddit.communities import communities_blueprint
@@ -7,11 +7,12 @@ from flaskeddit.models import Community, User
 
 @communities_blueprint.route("/communities")
 def communities():
+    page = int(request.args.get("page", 1))
     communities = (
         db.session.query(Community, User.username)
         .join(Community, Community.user_id == User.id)
         .order_by(Community.date_created.desc())
-        .all()
+        .paginate(page=page, per_page=5)
     )
     return render_template("communities.jinja2", page="recent", communities=communities)
 
@@ -19,10 +20,11 @@ def communities():
 @communities_blueprint.route("/communities/top")
 def top_communities():
     # TODO: Update to sort by most joined
+    page = int(request.args.get("page", 1))
     communities = (
         db.session.query(Community, User.username)
         .join(Community, Community.user_id == User.id)
         .order_by(Community.date_created.desc())
-        .all()
+        .paginate(page=page, per_page=1)
     )
     return render_template("communities.jinja2", page="top", communities=communities)
