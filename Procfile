5f57c55f352152db47871d3f8f8a1fae5835c8cd
Austin <cheungaustin2@gmail.com>
2019-06-09 08:56:11 -0500

    prepare for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn "flaskeddit:create_app()"
--- flaskeddit/config.py
+++ flaskeddit/config.py
@@ -3,8 +3,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", os.urandom(16))
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "SQLALCHEMY_DATABASE_URI", "sqlite:///app.db"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     USE_SESSION_FOR_NEXT = True
