493ccd7b9bc15a847c0ff7fbd9ed2aa84119c3b7
Austin <cheungaustin2@gmail.com>
2019-06-10 19:25:45 -0500

    use raw strings for regex

--- flaskeddit/auth/forms.py
+++ flaskeddit/auth/forms.py
@@ -26,17 +26,17 @@ class RegisterForm(FlaskForm):
             raise ValidationError("Username is taken.")
 
     def validate_password(self, password):
-        if re.search("\d", password.data) is None:
+        if re.search(r"\d", password.data) is None:
             raise ValidationError("Password should contain at least 1 number.")
-        elif re.search("[A-Z]", password.data) is None:
+        elif re.search(r"[A-Z]", password.data) is None:
             raise ValidationError(
                 "Password should contain at least 1 uppercase character."
             )
-        elif re.search("[a-z]", password.data) is None:
+        elif re.search(r"[a-z]", password.data) is None:
             raise ValidationError(
                 "Password should contain at least 1 lowercase character."
             )
-        elif re.search("\W", password.data) is None:
+        elif re.search(r"\W", password.data) is None:
             raise ValidationError(
                 "Password should contain at least 1 special character."
             )
