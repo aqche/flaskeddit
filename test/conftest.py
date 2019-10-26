1c0408c95c1ca273248cfbfddd857f5917d1a5f8
Austin <cheungaustin2@gmail.com>
2019-10-26 14:04:18 -0700

    update secret key default and update testing config

--- test/conftest.py
+++ test/conftest.py
@@ -10,6 +10,7 @@ def test_client():
     """
     app = create_app()
     app.app_context().push()
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
     app.config["WTF_CSRF_ENABLED"] = False
     db.create_all()
     yield app.test_client()
